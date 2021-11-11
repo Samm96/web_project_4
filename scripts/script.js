95dabc5cf22ccf75efc192b8a81d6cf2d66aba6b
Samm96 <sihorsch96@gmail.com>
2021-11-11 13:54:24 -0500

    feat: close form via background

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,27 @@
 const editProfilePopup = document.querySelector('#edit-popup-form');
 const editProfileButton = document.querySelector('.edit-button');
-
 const formCloseButton = document.querySelector('.close-button');
 
+
+//This opens and closes form
 editProfileButton.addEventListener('click', () => {
     editProfilePopup.classList.add('popup-form_open');
 })
-
 formCloseButton.addEventListener('click', () => {
     editProfilePopup.classList.remove('popup-form_open');
 })
+//close form by clicking anything but popup
+editProfilePopup.addEventListener('click', (e) => {
+    if(e.target === editProfilePopup){
+        editProfilePopup.classList.remove('popup-form_open');
+    }
+})
 
 
+//This makes the editProfileButton change color when hovered over
 editProfileButton.addEventListener('mouseover', () => {
-    editProfileButton.classList.add('.edit-button_hover');
+    editProfileButton.classList.add('edit-button_hover');
+})
+editProfileButton.addEventListener('mouseout', () => {
+        editProfileButton.classList.remove('edit-button_hover');
 })
