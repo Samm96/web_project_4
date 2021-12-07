c260b8b73be57a0ad5cdb1952405851cb0a80847
Samm96 <sihorsch96@gmail.com>
2021-12-07 14:11:48 -0500

    JS remove/add error styles attempt

--- scripts/validate.js
+++ scripts/validate.js
@@ -3,16 +3,8 @@ enableValidation({
   inputSelector: ".popup-form__input",
   submitButtonSelector: ".popup-form__submit-button",
   inactiveButtonClass: "popup-form__submit-button_type_disabled",
-  errorTextSelector: ".popup-form__error-text",
+  errorTextSelector: ".popup-form__error-text"
 });
-
-const errorMessage = {
-    error1: "Please fill out this field.",
-    error2: "Please enter a web address."
-};
-
-errorMessage.error1;
-errorMessage.error2;
 
 function enableValidation(settings) {
   const forms = document.querySelectorAll(settings.formSelector);
@@ -34,25 +26,19 @@ function setEventListeners(form, settings) {
 function checkInputValidity(input) {
   if (input.validity.valid) {
     removeErrorStyles(input);
-    hideErrorMessage();
   } else {
     addErrorStyles(input);
-    showErrorMessage();
   }
 }
 
 function removeErrorStyles(input) {
-    input.classList.remove("popup-form__input_has_error");
+  input.classList.remove("popup-form__input_has_error");
+  const errorText = document.querySelectorAll(settings.errorTextSelector);
+  errorText.classList.remove("popup-form__error-text_visible");
 }
 
-function addErrorStyles(input) {
+function addErrorStyles(input, errorText) {
     input.classList.add("popup-form__input_has_error");
+    errorText.classList.add("popup-form__error-text_visible");
 }
 
-function showErrorMessage () {
-    
-}
-
-function hideErrorMessage() {
-
-}
