9faa2366d82fe18f3a5a57fa10f82ed178498fec
Samm96 <sihorsch96@gmail.com>
2021-12-07 20:07:26 -0500

    HTML: prettier, added text to button / CSS: deleted unnecessary classes, replaced bkgrd img btn / indexcss / JS: validation script

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,11 +1,14 @@
 enableValidation({
   formSelector: ".popup-form__input-container",
   inputSelector: ".popup-form__input",
-  submitButtonSelector: ".popup-form__submit-button",
+  submitButtonSelector: ".submit-button",
   inactiveButtonClass: "popup-form__submit-button_type_disabled",
-  errorTextSelector: ".popup-form__error-text"
+  errorTextSelector: ".popup-form__error-text",
+  inputHasError: "popup-form__input_has_error",
+  errorTextVisible: "popup-form__error-text_visible",
 });
 
+//select all of the forms and add event listener for each one
 function enableValidation(settings) {
   const forms = document.querySelectorAll(settings.formSelector);
   forms.forEach((form) => {
@@ -13,32 +16,71 @@ function enableValidation(settings) {
   });
 }
 
+// add event listener function: it will use parameters form and settings.
+// select all of the inputs
+/// want to select submit button in ths fx too
+/// add a part that has a condition where there is an error and what to do then
+/// add button disable toggle
 function setEventListeners(form, settings) {
-  const input = form.querySelectorAll(settings.inputSelector);
+  const inputs = form.querySelectorAll(settings.inputSelector);
   //checks in put validity & check all inputs for validity
-  input.forEach((input) => {
+  inputs.forEach((input) => {
     input.addEventListener("input", (evt) => {
-      checkInputValidity(input);
+      checkInputValidity(input, settings);
+      const submitBtn = form.querySelector(settings.submitButtonSelector);
+      const hasErrors = notValid(inputs);
+      setButton(submitBtn, hasErrors);
     });
   });
 }
 
-function checkInputValidity(input) {
-  if (input.validity.valid) {
-    removeErrorStyles(input);
+// toggles the button between disabled and enabled
+function setButton (submitBtn, hasErrors) {
+  if (hasErrors) {
+    submitBtn.disabled = true;
   } else {
-    addErrorStyles(input);
+    submitBtn.disabled = false;
   }
 }
 
-function removeErrorStyles(input) {
-  input.classList.remove("popup-form__input_has_error");
+// turn inputs into an array, take some input; return solution if input is invalid
+function notValid (inputs) {
+  return Array.from(inputs).some((input) => {
+    return !input.validity.valid;
+  });
+}
+
+// removes/hides errors if inputs are valid
+function checkInputValidity(input, settings) {
+  if (input.validity.valid) {
+    removeErrorStyles(input, settings);
+    hideErrorMessage(input, settings);
+  } else {
+    addErrorStyles(input, settings);
+    showErrorMessage(input, settings);
+  }
+}
+
+function removeErrorStyles(input, settings) {
+  input.classList.remove(settings.inputHasError);
   const errorText = document.querySelectorAll(settings.errorTextSelector);
-  errorText.classList.remove("popup-form__error-text_visible");
 }
 
-function addErrorStyles(input, errorText) {
-    input.classList.add("popup-form__input_has_error");
-    errorText.classList.add("popup-form__error-text_visible");
+function hideErrorMessage (input, settings) {
+  const errorMessageElement = document.querySelector(`.${input.id}-error`);
+  errorMessageElement.classList.remove(settings.errorTextVisible);
 }
 
+function addErrorStyles(input, settings) {
+    input.classList.add(settings.inputHasError);
+}
+
+function showErrorMessage (input, settings) {
+  const errorMessageElement = document.querySelector(`.${input.id}-error`);
+  errorMessageElement.textContent = input.validationMessage;
+  errorMessageElement.classList.add(settings.errorTextVisible);
+}
+
+
+// to check to see if something is being grabbed correctly:
+/// via dev tools: i.e. type "input.id" to see which input function is grabbing.
