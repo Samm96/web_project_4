e1fdb8eb6ec341c5e382d687b5a5d62265833222
Samm96 <sihorsch96@gmail.com>
2022-01-26 18:56:37 -0500

    Refactor: new constants.js file, deleted unnecessary code & utils.js, import/export constants.js, fix initialCards img path

---
+++ src/scripts/Constants.js
@@ -0,0 +1,55 @@
+//popup-forms
+export const createCardForm = document.querySelector("#create");
+
+//buttons
+export const editProfileButton = document.querySelector(".edit-button");
+export const addCardButton = document.querySelector(".add-button");
+
+//
+export const profileName = document.querySelector(".profile__name");
+export const profileJob = document.querySelector(".profile__description");
+
+//inputs
+export const inputName = document.querySelector("#name");
+export const inputJob = document.querySelector("#description");
+
+//cardtemplate
+export const cardTemplate = document.querySelector("#card-template");
+
+//local images
+const Tramway = require('../images/Palm-Springs-Aerial-Tramway-colorfix.jpg');
+const CedarPt = require('../images/Corkscrew_(Cedar_Point)_01.jpg');
+const PowWow = require('../images/jess-lindner-xegyDn1-SoY-unsplash-colorfix.jpg');
+const ColWilliamsburg = require('../images/Colonial-Williamsburg-Virginiasizefix-2.jpg');
+const PuertoRico = require('../images/jennifer-chen-yXJiC2UjIBo-unsplash-colorfix.jpg');
+const DisneyWorld = require('../images/sandro-gonzalez-dBEJG6hv224-unsplash.jpg');
+
+
+//loop for initial cards
+export const initialCards = [
+  {
+    title: "Palm Springs Aerial Tramway",
+    url: Tramway,
+  },
+  {
+    title: "Cedar Point, OH",
+    url: CedarPt,
+  },
+  {
+    title: "N.A. Pow Wows",
+    url: PowWow,
+  },
+  {
+    title: "Colonial Williamsburg",
+    url: ColWilliamsburg,
+  },
+  {
+    title: "Old San Juan, Puerto Rico",
+    url: PuertoRico,
+  },
+  {
+    title: "Disney World",
+    url: DisneyWorld,
+  },
+];
+
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,63 +7,18 @@ import PopupWithImage from "./PopupWithImage.js";
 import PopupWithForm from "./PopupWithForm.js";
 import UserInfo from "./UserInfo.js";
 
-//popup-forms
-const createCardPopup = document.querySelector("#create-popup-form");
-const imageCardPopup = document.querySelector("#image-popup");
-const editProfilePopup = document.querySelector("#edit-popup-form");
-const popupForms = document.querySelectorAll(".popup-form");
+import {
+  createCardForm,
+  editProfileButton,
+  addCardButton,
+  profileName,
+  profileJob,
+  inputName,
+  inputJob,
+  cardTemplate,
+  initialCards,
+} from "./Constants.js";
 
-const popupImageElement = imageCardPopup.querySelector("#imgPopupImg");
-const popupCaption = imageCardPopup.querySelector(".popup-form__caption");
-const createCardForm = document.querySelector("#create");
-
-
-//buttons
-const editProfileButton = document.querySelector(".edit-button");
-const addCardButton = document.querySelector(".add-button");
-
-//
-const profileName = document.querySelector(".profile__name");
-const profileJob = document.querySelector(".profile__description");
-
-//inputs
-const inputName = document.querySelector("#name");
-const inputJob = document.querySelector("#description");
-
-//cardtemplate
-const cardTemplate = document.querySelector("#card-template");
-const cardSection = document.querySelector(".elements");
-
-//loop for initial cards
-const initialCards = [
-  {
-    title: "Palm Springs Aerial Tramway",
-    url: "images/Palm-Springs-Aerial-Tramway-colorfix.jpg",
-  },
-  {
-    title: "Cedar Point, OH",
-    url: "images/Corkscrew_(Cedar_Point)_01.jpg",
-  },
-  {
-    title: "N.A. Pow Wows",
-    url: "images/jess-lindner-xegyDn1-SoY-unsplash-colorfix.jpg",
-  },
-  {
-    title: "Colonial Williamsburg",
-    url: "images/Colonial-Williamsburg-Virginiasizefix-2.jpg",
-  },
-  {
-    title: "Old San Juan, Puerto Rico",
-    url: "images/jennifer-chen-yXJiC2UjIBo-unsplash-colorfix.jpg",
-  },
-  {
-    title: "Disney World",
-    url: "images/sandro-gonzalez-dBEJG6hv224-unsplash.jpg",
-  },
-];
-
-
-//functions called
 
 const validationConfig = {
   formSelector: ".popup-form__input-container",
@@ -75,44 +30,48 @@ const validationConfig = {
   errorTextVisible: "popup-form__error-text_visible",
 };
 
-const imagePopup = new PopupWithImage ("image-popup");
+const imagePopup = new PopupWithImage("image-popup");
 
 const createCard = (data) => {
-  const card = new Card ({
-    data,
-    handleCardClick: () => {
-      imagePopup.open(data);
+  const card = new Card(
+    {
+      data,
+      handleCardClick: () => {
+        imagePopup.open(data);
+      },
     },
-  },
-  cardTemplate
+    cardTemplate
   );
   return card.render();
-}
+};
 
-const cardList = new Section({
-  items: initialCards,
-  renderer: (data) => {
-    cardList.addItem(createCard(data));
+const cardList = new Section(
+  {
+    items: initialCards,
+    renderer: (data) => {
+      cardList.addItem(createCard(data));
+    },
   },
-}, "elements");
+  "elements"
+);
 
 const userInfo = new UserInfo({
   nameSelector: "profile__name",
-  descriptionSelector: "profile__description", 
+  descriptionSelector: "profile__description",
 });
 
 const editProfilePopupForm = new PopupWithForm({
   popupSelector: "edit-popup-form",
   handleFormSubmit: (data) => {
     userInfo.setUserInfo(data);
-  }
+  },
 });
 
 const createCardPopupForm = new PopupWithForm({
   popupSelector: "create-popup-form",
   handleFormSubmit: (data) => {
     cardList.addItem(createCard(data));
-  }
+  },
 });
 
 const editProfileValidator = new FormValidator(
@@ -144,15 +103,3 @@ cardList.renderItem(initialCards);
 imagePopup.setEventListeners();
 editProfilePopupForm.setEventListeners();
 createCardPopupForm.setEventListeners();
-
-export {
-  imageCardPopup,
-  createCardPopup,
-  editProfilePopup,
-  inputName,
-  inputJob,
-  profileJob,
-  profileName,
-  popupImageElement,
-  popupCaption
-};
