30c897de45e673be4363067f7773e77cdb111043
Samm96 <sihorsch96@gmail.com>
2022-01-26 19:06:52 -0500

    Add: postcss config

--- webpack.config.js
+++ webpack.config.js
@@ -35,8 +35,10 @@ module.exports = {
                 use: [
                     MiniCssExtractPlugin.loader,
                     {
-                        loader: "css-loader"
+                        loader: "css-loader",
+                        options: { importLoaders: 1 }
                     },
+                    "postcss-loader"
                 ],
             },
             {
