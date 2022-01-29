6a79b5fb3a88f9bfd166b0061c0efb335ac0cc61
Samm96 <sihorsch96@gmail.com>
2022-01-28 22:19:12 -0500

    Renamed UserInfo this._name and this._description to have element on end

--- src/scripts/UserInfo.js
+++ src/scripts/UserInfo.js
@@ -1,18 +1,18 @@
 export default class UserInfo {
   constructor({ nameSelector, descriptionSelector }) {
-    this._name = document.querySelector(`.${nameSelector}`);
-    this._description = document.querySelector(`.${descriptionSelector}`);
+    this._nameElement = document.querySelector(`.${nameSelector}`);
+    this._descriptionElement = document.querySelector(`.${descriptionSelector}`);
   }
   //returns an object with info about the user
   getUserInfo() {
     return {
-      name: this._name.textContent,
-      description: this._description.textContent,
+      name: this._nameElement.textContent,
+      description: this._descriptionElement.textContent,
     };
   }
   //takes new user data and adds it on the page
   setUserInfo({ name, description }) {
-    this._name.textContent = name;
-    this._description.textContent = description;
+    this._nameElement.textContent = name;
+    this._descriptionElement.textContent = description;
   }
 }
